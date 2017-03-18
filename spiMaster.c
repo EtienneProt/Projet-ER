eb6b25be6ecc8870e0fdfac4ec0b73220b028632
M3103DB2 <etienneprot10@gmail.com>
2017-03-18 12:06:48 +0100

    Add files via upload

---
+++ spiMaster.c
@@ -0,0 +1,35 @@
+//SPI master
+#include <avr/io.h>
+#include <util/delay.h>
+
+//SPI initvoid
+void SPIMasterInit(void) {
+//set MOSI, SCK and SS as output
+  DDRB |= (1<<PB3)|(1<<PB5)|(1<<PB2);
+//set SS to high
+  PORTB |= (1<<PB2);
+//enable master SPI at clock rate Fck/16
+  SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+//master send function
+void SPIMasterSend(uint8_t data){
+//select slave
+  PORTB &= ~(1<<PB2);
+//send data
+  SPDR=data;
+//wait for transmition complete
+  while (!(SPSR &(1<<SPIF)));
+//SS to high
+  PORTB |= (1<<PB2);
+}
+
+int main(void) {
+//initialize master SPI
+  SPIMasterInit();
+//initial PWM value
+  uint8_t pwmval = 0;
+  while (1) {
+    SPIMasterSend(pwmval+=5);
+    _delay_ms(1000);
+  }
+}
---
+++ spiSlave.c
@@ -0,0 +1,44 @@
+//SPI slave
+#include <avr/io.h>
+#include <avr/interrupt.h>
+//SPI init
+void SPISlaveInit(void) {
+//set MISO as output
+  DDRB |= (1<<PB4);
+//enable SPI and enable SPI interrupt
+  SPCR = (1<<SPE)|(1<<SPIE);
+}
+void InitPort(void) {
+//set PD6 (OC0A) as output
+  DDRD|=(1<<PD6);
+}
+//Initialize Timer0
+void InitTimer0(void) {
+//Set Initial Timer value
+  TCNT0=0;
+//Place compare value to Output compare register
+  OCR0A=0;
+//Set fast PWM mode
+//and make clear OC0A on compare match
+  TCCR0A|=(1<<COM0A1)|(1<<WGM01)|(1<<WGM00);
+}
+void StartTimer0(void) {
+//Set prescaller 64 and start timer
+  TCCR0B|=(1<<CS01)|(1<<CS00);
+}
+ISR(SPI_STC_vect) {
+  OCR0A=SPDR;
+}
+
+int main(void) {
+//initialize slave SPI
+  SPISlaveInit();
+  InitPort();
+  InitTimer0();
+  StartTimer0();
+  sei();
+  while (1) {
+    //loop
+  }
+  return 0;
+}
