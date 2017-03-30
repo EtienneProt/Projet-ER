ea6bff8d44f8b13cc5e40cacfc2f232d0cdcee72
Etienne PROT <etienneprot10@gmail.com>
2017-03-30 11:22:42 +0200

    Add files via upload

---
+++ sketch_mar28c.ino
@@ -0,0 +1,28 @@
+const char led = 9;
+
+int w;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(led, OUTPUT) ;
+}
+
+void loop() {
+  recevoir();
+  if (w == 1) {
+    digitalWrite(led, HIGH);
+  }
+  if (w == 0) {
+    digitalWrite(led, LOW);
+  }
+  delay(500);
+}
+
+void recevoir() {
+  if (Serial.available()) {
+    w = Serial.read();
+    Serial.println(w);
+    //Serial.flush();
+  }
+}
+
